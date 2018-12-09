c2d6567ab44bc0d074c0bc53a2689ec189263df7
babie <babie.wildheart@gmail.com>
2018-12-09 21:00:43 +0900

    refactor: Agregate module exportings to `App`

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import { app } from 'hyperapp'
 import { location } from '@hyperapp/router'
 
-import { Modules as M } from './modules'
 import { App as A } from './App'
 
 const container = document.getElementById('app')
 let main
 if (process.env.NODE_ENV === 'development' && module.hot) {
   main = require('@hyperapp/logger').withLogger(app)(
-    M.state,
-    M.actions,
+    A.state,
+    A.actions,
     A.view,
     container
   )
 } else {
-  main = app(M.state, M.actions, A.view, container)
+  main = app(A.state, A.actions, A.view, container)
 }
 const unsubscribe = location.subscribe(main.location)
