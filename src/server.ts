b029b367dffbf89641a749e5557664d502c1f64c
babie <babie.wildheart@gmail.com>
2018-12-09 20:42:31 +0900

    refactor: Remove `jsdom-global` package

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,30 @@
-require('jsdom-global')('', {
-  url: 'http:/localhost:3000'
+import { JSDOM } from 'jsdom'
+
+declare var global: any
+global.document = new JSDOM('<!doctype html><html><body></body></html>', {
+  url: 'http://localhost:3000'
 })
+global.window = global.document.window
+global.navigator = { userAgent: 'node.js' }
+
 import { renderToStream } from '@hyperapp/render'
 import express from 'express'
 
-import { App as A } from './App'
+import { App } from './App'
 import { Html } from './Html'
 
 const server = express()
 
-server.use(express.static('dist'))
+server.use(express.static(__dirname + '/public'))
 
 server.get('/*', (req, res, next) => {
-  A.state.location.pathname = req.path
+  App.state.location.pathname = req.path
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  renderToStream(Html, A.state, A.actions).pipe(res)
+  renderToStream(Html, App.state, App.actions).pipe(res)
   next()
 })
 
-server
-  .listen(3000, () => {
-    console.log(`Server is running.`)
-  })
-  .on('error', console.error)
+server.listen(3000, () => {
+  console.log(`Server is running.`)
+})
